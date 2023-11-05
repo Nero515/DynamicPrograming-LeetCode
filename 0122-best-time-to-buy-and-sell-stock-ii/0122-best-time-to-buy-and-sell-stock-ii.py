9b58971b684570ad5cbdcb4259e970568f48a3d1
Nero515 <63355616+Nero515@users.noreply.github.com>
2023-11-05 16:39:53 +0100

    Time: 65 ms (52.67%), Space: 17.8 MB (9.05%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,17 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ans,flag,buy = 0, 0, 10 ** 10
+        ans, flag, buy = 0, 0, 10**10
         if len(prices) <= 1:
             return 0
-        for i in range(len(prices) - 1):
-            if prices[i + 1] > prices[i]:
-                if not flag:
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i] and not flag:
                     flag = 1
                     buy = prices[i]
             else:
                 if flag:
-                    flag = 0
                     ans += prices[i] - buy
-                    buy = 10 **  10
-        ans += (prices[i + 1] - buy > 0) * (prices[i + 1] - buy)
+                    if prices[i+1] > prices[i]:
+                        buy = prices[i]
+                        flag = 1
+                    else:
+                        flag = 0
+                        buy = 10 ** 10
+        if prices[i+1] > prices[i]:
+            ans += prices[i+1] - buy
         return ans
+                    
