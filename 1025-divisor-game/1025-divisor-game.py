1ee21222b9ba722ae0f226a216b77949e5f32761
Nero515 <63355616+Nero515@users.noreply.github.com>
2023-11-05 14:30:26 +0100

    Time: 35 ms (80.44%), Space: 17.1 MB (7.64%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -2,13 +2,7 @@ class Solution:
     def divisorGame(self, n: int) -> bool:
         if n == 1:
             return False
-        
-        def get_x(n: int) -> int:
-            for i in range(1, n):
-                if n % i == 0:
-                    return i
-        
-        x = get_x(n)
+        x = 1
         new_n = n - x
         flag = self.divisorGame(new_n)
         if flag == False:
